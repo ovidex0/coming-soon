7cd21cfc90cadf7f293aefade0aed62fe359dc92
Ovidex0 <ovidex@protonmail.com>
2021-11-29 15:30:25 +0200

    init

--- gatsbyjs/src/pages/index.js
+++ gatsbyjs/src/pages/index.js
@@ -1,48 +1,31 @@
 import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'gatsby';
-import { Icon } from 'react-icons-kit';
-import Modal from 'react-modal';
-import { location } from 'react-icons-kit/icomoon/location';
-import { phone } from 'react-icons-kit/icomoon/phone';
-import { home } from 'react-icons-kit/entypo/home';
-import { x } from 'react-icons-kit/feather/x';
 import LanguageProvider from '@deadline/common/LanguageSwitcher/context/language.provider';
 import LanguageSwitcher from '@deadline/common/LanguageSwitcher';
 import languageConfig from '@deadline/common/LanguageSwitcher/config';
-import NormalClock from '@deadline/components/NormalClock/NormalClock.js';
-import Button from '@deadline/components/Button';
-import ContactForm from '@deadline/components/MaterialContactFormTwo/MaterialContactForm';
-import SubscribeModal from '@deadline/components/SubscribeModal/SubscribeModal';
-import MainContentWrapper, {
+import NormalClock from '@deadline/components/NormalClock/NormalClock';
+import MainWrapper, {
   MainContentSection,
   NormalClockWrapper,
-  LogoImageContainer,
-  ButtonWrapper,
-  MainWrapper,
-  SideBar,
-  Overlay,
-  SidebarContent,
-  About,
-  Contact,
-  Info,
-  InfoItem,
-  InfoIcon,
-  SidebarClose,
   FooterSection,
-  ContactButton,
-  ModalStyle,
-} from '@deadline/common/ui/fourteen.style';
+  ContactFormWrap,
+  LogoImageContainer,
+  ContentWrapper,
+  NotifyButton,
+} from '@deadline/common/ui/sixteen.style';
 import LogoImage from '@deadline/common/static/images/logoTwo.png';
+import { SOCIAL_PROFILES } from '@deadline/common/data/social-share/sixteen';
 // Language translation files
-import localEng from '@deadline/common/data/translation/fourteen/en.json';
-import localAr from '@deadline/common/data/translation/fourteen/ar.json';
-import localEs from '@deadline/common/data/translation/fourteen/es.json';
-import localDe from '@deadline/common/data/translation/fourteen/de.json';
-import localCn from '@deadline/common/data/translation/fourteen/zh.json';
-import localIl from '@deadline/common/data/translation/fourteen/he.json';
-import { SOCIAL_PROFILES } from '@deadline/common/data/social-share/fourteen';
+import localEng from '@deadline/common/data/translation/sixteen/en.json';
+import localAr from '@deadline/common/data/translation/sixteen/ar.json';
+import localEs from '@deadline/common/data/translation/sixteen/es.json';
+import localDe from '@deadline/common/data/translation/sixteen/de.json';
+import localCn from '@deadline/common/data/translation/sixteen/zh.json';
+import localIl from '@deadline/common/data/translation/sixteen/he.json';
 import { Container, SocialShare, Seo } from '../components';
+import ContactForm from '@deadline/components/ContactForm/ContactForm';
+import Button from '@deadline/components/Button';
 // Language translation Config
 const messages = {
   en: localEng,
@@ -56,135 +39,61 @@ const messages = {
 const deadline = new Date(Date.parse(new Date()) + 12 * 24 * 60 * 60 * 1000);
 
 const IndexPage = () => {
-  const [state, setState] = useState({
-    toggle: false,
-  });
-
-  const toggleHandle = () => {
-    setState({
-      ...state,
-      toggle: !state.toggle,
-    });
+  const [visible, setVisible] = useState(false);
+  const toggleContact = () => {
+    setVisible(true);
   };
-
-  const [modalIsOpen, setIsOpen] = React.useState(false);
-
-  function closeModal() {
-    setIsOpen(false);
-  }
 
   return (
     <LanguageProvider messages={messages}>
       <React.Fragment>
         <Seo title="title" />
-        <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          ariaHideApp={false}
-          contentLabel="Selected Option"
-          className="newsletter-modal"
-        >
-          <SubscribeModal closeHandler={closeModal} />
-        </Modal>
-        <ModalStyle />
         <MainWrapper>
-          <MainContentWrapper>
-            <LogoImageContainer>
-              <Link to={'/fourteen'}>
-                <img src={LogoImage} alt="logo" />
-              </Link>
-              <ContactButton type="submit" onClick={toggleHandle}>
-                <FormattedMessage id="contactBtnText" />
-              </ContactButton>
-            </LogoImageContainer>
-            <Container className="mainContainer">
-              <MainContentSection>
+          <LogoImageContainer>
+            <Link to={'/three'}>
+              <img src={LogoImage} alt="logo" />
+            </Link>
+          </LogoImageContainer>
+          <Container className="mainContainer">
+            <MainContentSection>
+              <NormalClockWrapper>
+                <NormalClock countdown={deadline} />
+              </NormalClockWrapper>
+              <ContentWrapper>
+                <p>
+                  <FormattedMessage id="ticker" />
+                </p>
                 <h2>
                   <FormattedMessage id="mainMessage" />
                 </h2>
-                <p>
-                  <FormattedMessage id="description" />
-                </p>
-                <NormalClockWrapper>
-                  <NormalClock countdown={deadline} />
-                </NormalClockWrapper>
-                <ButtonWrapper>
-                  <Button
-                    type="submit"
-                    title="modalButtonText"
-                    onClick={() => setIsOpen(true)}
-                  />
-                </ButtonWrapper>
-              </MainContentSection>
-            </Container>
-            <FooterSection>
-              <SocialShare items={SOCIAL_PROFILES} />
-              <p>
-                <FormattedMessage id="copyrightText" />
-              </p>
-            </FooterSection>
-          </MainContentWrapper>
-          <SideBar className={state.toggle === true ? 'expand' : ''}>
-            <SidebarContent>
-              <SidebarClose type="submit" aria-label="close">
-                <Icon icon={x} size={33} onClick={toggleHandle} />
-              </SidebarClose>
-              <About>
-                <h2>
-                  <FormattedMessage id="aboutTitle" />
-                </h2>
-                <p>
-                  <FormattedMessage id="aboutText" />
-                </p>
-              </About>
+                {visible ? (
+                  <ContactFormWrap>
+                    <ContactForm />
+                  </ContactFormWrap>
+                ) : (
+                  ''
+                )}
 
-              <Contact>
-                <h2>
-                  <FormattedMessage id="contactTitle" />
-                </h2>
-                <ContactForm />
-              </Contact>
-
-              <Info>
-                <InfoItem>
-                  <InfoIcon>
-                    <Icon icon={location} size={33} />
-                  </InfoIcon>
-                  <h4>
-                    <FormattedMessage id="addressTitle" />
-                  </h4>
-                  <p>
-                    <FormattedMessage id="addressDetails" />
-                  </p>
-                </InfoItem>
-
-                <InfoItem>
-                  <InfoIcon>
-                    <Icon icon={phone} size={32} />
-                  </InfoIcon>
-                  <h4>
-                    <FormattedMessage id="phoneTitle" />
-                  </h4>
-                  <p>
-                    <FormattedMessage id="phoneNumbers" />
-                  </p>
-                </InfoItem>
-
-                <InfoItem>
-                  <InfoIcon>
-                    <Icon icon={home} size={32} />
-                  </InfoIcon>
-                  <h4>
-                    <FormattedMessage id="websiteTitle" />
-                  </h4>
-                  <p>
-                    <FormattedMessage id="Websites" />
-                  </p>
-                </InfoItem>
-              </Info>
-            </SidebarContent>
-            <Overlay className={state.toggle === true ? 'expand' : ''} />
-          </SideBar>
+                {!visible ? (
+                  <NotifyButton>
+                    <Button
+                      type="submit"
+                      title="notifyText"
+                      onClick={toggleContact}
+                    />
+                  </NotifyButton>
+                ) : (
+                  ''
+                )}
+              </ContentWrapper>
+            </MainContentSection>
+          </Container>
+          <FooterSection>
+            <SocialShare items={SOCIAL_PROFILES} />
+            <p>
+              <FormattedMessage id="copyrightText" />
+            </p>
+          </FooterSection>
         </MainWrapper>
         <LanguageSwitcher languageConfig={languageConfig} />
       </React.Fragment>
